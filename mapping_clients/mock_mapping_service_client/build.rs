045ca394cc19e7b81b487efa7aa6d27e6f041302
William Lyles <26171886+wilyle@users.noreply.github.com>
2023-09-27 13:33:00 -0700

    make the changes to the other mapping client

--- mapping_clients/mock_mapping_service_client/build.rs
+++ mapping_clients/mock_mapping_service_client/build.rs
@@ -2,20 +2,19 @@
 // Licensed under the MIT license.
 // SPDX-License-Identifier: MIT
 
-use std::{env, fs, path::Path};
+use std::env;
 
-const OUT_DIR: &str = "OUT_DIR";
-const SAMPLE_CONFIG_FILE: &str = "res/mock_mapping_service_client_config.sample.json";
-const CONFIG_FILE: &str = "mock_mapping_service_client_config.json";
+use freyja_build_common::copy_to_build_out_dir;
+
+const RES_DIR_NAME: &str = "res";
+const DEFAULT_CONFIG_FILE: &str = "mock_mapping_client_config.default.json";
 
 fn main() {
-    // The current directory of the build script is the package's root directory
-    let config_path = env::current_dir().unwrap().join(SAMPLE_CONFIG_FILE);
+    // Current directory of the build script is the package's root directory
+    let config_path = env::current_dir()
+        .unwrap()
+        .join(RES_DIR_NAME)
+        .join(DEFAULT_CONFIG_FILE);
 
-    let target_dir = env::var(OUT_DIR).unwrap();
-    let dest_path = Path::new(&target_dir).join(CONFIG_FILE);
-
-    fs::copy(&config_path, dest_path).unwrap();
-
-    println!("cargo:rerun-if-changed={}", config_path.to_str().unwrap());
+    copy_to_build_out_dir(config_path, DEFAULT_CONFIG_FILE);
 }
--- mapping_clients/mock_mapping_service_client/src/lib.rs
+++ mapping_clients/mock_mapping_service_client/src/lib.rs
@@ -2,5 +2,5 @@
 // Licensed under the MIT license.
 // SPDX-License-Identifier: MIT
 
+mod config;
 pub mod mock_mapping_service_client;
-mod mock_mapping_service_client_config;
