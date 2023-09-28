e8e7140a6e61cbfd20ce5729b008397e4e924e31
William Lyles <26171886+wilyle@users.noreply.github.com>
2023-09-28 14:32:07 -0700

    change common too

--- common/src/config_utils.rs
+++ common/src/config_utils.rs
@@ -13,18 +13,18 @@ const DOT_FREYJA_DIR: &str = ".freyja";
 const FREYJA_HOME: &str = "FREYJA_HOME";
 
 /// Read config from layered configuration files.
-/// Uses `{config_file_name}.default.{config_file_ext}` as the base configuration,
-/// then searches for overrides named `{config_file_name}.{config_file_ext}` in the current directory and `$FREYJA_HOME`.
+/// Uses `{config_file_stem}.default.{config_file_ext}` as the base configuration,
+/// then searches for overrides named `{config_file_stem}.{config_file_ext}` in the current directory and `$FREYJA_HOME`.
 /// If `$FREYJA_HOME` is not set, it defaults to `$HOME/.freyja`.
 ///
 /// # Arguments
-/// - `config_file_name`: The config file name without an extension. This is used to construct the file names to search for
+/// - `config_file_stem`: The config file name without an extension. This is used to construct the file names to search for
 /// - `config_file_ext`: The config file extension. This is used to construct the file names to search for
 /// - `default_config_path`: The path to the directory containing the default configuration
 /// - `io_error_handler`: The error handler for `std::io::Error` errors
 /// - `config_error_handler`: The error handler for errors from the config library
 pub fn read_from_files<TConfig, TError, TPath, TIoErrorHandler, TConfigErrorHandler>(
-    config_file_name: &str,
+    config_file_stem: &str,
     config_file_ext: &str,
     default_config_path: TPath,
     io_error_handler: TIoErrorHandler,
@@ -36,10 +36,10 @@ where
     TIoErrorHandler: Fn(std::io::Error) -> TError,
     TConfigErrorHandler: Fn(ConfigError) -> TError,
 {
-    let default_config_filename = format!("{config_file_name}.default.{config_file_ext}");
+    let default_config_filename = format!("{config_file_stem}.default.{config_file_ext}");
     let default_config_file = default_config_path.as_ref().join(default_config_filename);
 
-    let overrides_filename = format!("{config_file_name}.{config_file_ext}");
+    let overrides_filename = format!("{config_file_stem}.{config_file_ext}");
 
     // The path below resolves to {current_dir}/{overrides_filename}
     let current_dir_config_path = env::current_dir()
