475a78caf477ee297efb2cd4123b7012c3cf29da
William Lyles <26171886+wilyle@users.noreply.github.com>
2023-09-29 13:58:13 -0700

    remove migrated code

--- cloud_connectors/azure/proto-build/build.rs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-// SPDX-License-Identifier: MIT
-
-fn main() {
-    tonic_build::compile_protos("../proto/azure_cloud_connector.proto").unwrap();
-}
--- cloud_connectors/azure/proto-build/src/lib.rs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-// SPDX-License-Identifier: MIT
-
-pub mod azure_cloud_connector {
-    tonic::include_proto!("azure_cloud_connector");
-}
