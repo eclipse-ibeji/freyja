2346383af57cad2080b07715acc940c94f654e97
William Lyles <26171886+wilyle@users.noreply.github.com>
2024-01-22 14:44:03 -0800

    re-export tokio isn't working so just remove it

--- proc_macros/src/freyja_main/generate.rs
+++ proc_macros/src/freyja_main/generate.rs
@@ -25,7 +25,7 @@ pub(crate) fn generate(ir: FreyjaMainOutput) -> TokenStream {
     } = ir;
 
     quote! {
-        #[::freyja::tokio::main]
+        #[tokio::main]
         async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             use freyja::freyja_common::data_adapter::DataAdapterFactory;
             let factories: Vec<Box<dyn DataAdapterFactory + Send + Sync>> = vec![
