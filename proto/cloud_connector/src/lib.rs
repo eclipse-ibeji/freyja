1d5520235d1ec002cf86475da69fd57941c83be5
William Lyles <26171886+wilyle@users.noreply.github.com>
2024-02-09 11:06:43 -0800

    comments

--- proto/cloud_connector/src/lib.rs
+++ proto/cloud_connector/src/lib.rs
@@ -21,14 +21,17 @@ pub mod v1 {
     }
 
     impl UpdateDigitalTwinRequestBuilder {
+        /// Create a new `UpdateDigitalTwinRequestBuilder` with default values
         pub fn new() -> Self {
             Self::default()
         }
 
+        /// Build the request
         pub fn build(self) -> UpdateDigitalTwinRequest {
             self.request
         }
 
+        /// Set the request value to null
         pub fn null_value(mut self) -> Self {
             self.request.value = Some(Value {
                 kind: Some(Kind::NullValue(0)),
@@ -37,6 +40,10 @@ pub mod v1 {
             self
         }
 
+        /// Set the request value to a number
+        /// 
+        /// # Arguments
+        /// - `n`: the value to set
         pub fn number_value(mut self, n: f64) -> Self {
             self.request.value = Some(Value {
                 kind: Some(Kind::NumberValue(n)),
@@ -45,6 +52,10 @@ pub mod v1 {
             self
         }
 
+        /// Set the request value to a string
+        /// 
+        /// # Arguments
+        /// - `s`: the value to set
         pub fn string_value(mut self, s: String) -> Self {
             self.request.value = Some(Value {
                 kind: Some(Kind::StringValue(s)),
@@ -53,6 +64,10 @@ pub mod v1 {
             self
         }
 
+        /// Set the request value to a boolean
+        /// 
+        /// # Arguments
+        /// - `b`: the value to set
         pub fn bool_value(mut self, b: bool) -> Self {
             self.request.value = Some(Value {
                 kind: Some(Kind::BoolValue(b)),
@@ -61,11 +76,16 @@ pub mod v1 {
             self
         }
 
+        /// Set the request timestamp
+        /// 
+        /// # Arguments
+        /// - `timestamp`: the timestamp to set
         pub fn timestamp(mut self, timestamp: Timestamp) -> Self {
             self.request.timestamp = Some(timestamp);
             self
         }
 
+        /// Set the request timestamp to the current time
         pub fn timestamp_now(mut self) -> Self {
             let timestamp = OffsetDateTime::now_utc();
             self.request.timestamp = Some(
@@ -83,11 +103,20 @@ pub mod v1 {
             self
         }
 
+        /// Set the request metadata. This overwrites any previously set metadata
+        /// 
+        /// # Arguments
+        /// - `metadata`: the metdata to set
         pub fn metadata(mut self, metadata: HashMap<String, String>) -> Self {
             self.request.metadata = metadata;
             self
         }
 
+        /// Add an entry to the request metadata
+        /// 
+        /// # Arguments
+        /// - `key`: the key of the metadata entry
+        /// - `value`: the value of the metadata entry
         pub fn add_metadata(mut self, key: String, value: String) -> Self {
             self.request.metadata.insert(key, value);
             self
