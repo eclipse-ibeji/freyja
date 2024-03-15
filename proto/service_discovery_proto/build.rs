90f6654ec4235984ab015609f03b7961ae8539a6
William Lyles <26171886+wilyle@users.noreply.github.com>
2024-03-15 14:01:36 -0700

    target specific versions for protobuf

--- proto/service_discovery_proto/build.rs
+++ proto/service_discovery_proto/build.rs
@@ -3,13 +3,11 @@
 // SPDX-License-Identifier: MIT
 
 use freyja_build_common::compile_remote_proto;
-
-const CHARIOTT_SERVICE_DISCOVERY_INTERFACES_BASE_URI: &str =
-    "https://raw.githubusercontent.com/eclipse-chariott/chariott/main/service_discovery/proto";
+use proto_common::interface_url;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     compile_remote_proto(
-        format!("{CHARIOTT_SERVICE_DISCOVERY_INTERFACES_BASE_URI}/core/v1/service_registry.proto"),
+        interface_url!(chariott, SERVICE_REGISTRY_INTERFACE),
         &[],
     )?;
 
