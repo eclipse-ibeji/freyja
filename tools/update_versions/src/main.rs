ec7153bb346ec3f3f8b418c6e6ac87a3d62ed103
William Lyles <26171886+wilyle@users.noreply.github.com>
2024-03-19 14:54:38 -0700

    initial attempt

---
+++ tools/update_versions/src/main.rs
@@ -0,0 +1,61 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+// SPDX-License-Identifier: MIT
+
+use std::{env, fs};
+
+use glob::glob;
+
+use freyja_common::cmd_utils::parse_args;
+use log::{error, info};
+use toml_edit::{value, DocumentMut};
+
+fn main() {
+    let args = parse_args(env::args()).expect("Failed to parse args");
+
+    let dir = match args.get("dir") {
+        Some(Some(d)) => d,
+        Some(None) => panic!("--dir argument missing value"),
+        None => ".",
+    };
+
+    let version = match args.get("version") {
+        Some(Some(v)) => v,
+        _ => panic!("Missing required argument: --version"),
+    };
+
+    let dry_run = args.contains_key("dry-run");
+
+    for file in glob(&format!("{}/**/Cargo.toml", dir)).expect("Failed to read glob pattern") {
+        match file {
+            Err(e) => error!("Unable to read file: {e}"),
+            Ok(path) => {
+                info!("Updating {:?}...", path.display());
+                if !dry_run {
+                    let contents = match fs::read_to_string(&path) {
+                        Ok(s) => s,
+                        Err(e) => {
+                            error!("\tError reading file: {e}");
+                            continue;
+                        }
+                    };
+
+                    let mut toml = match contents.parse::<DocumentMut>() {
+                        Ok(d) => d,
+                        Err(e) => {
+                            error!("\tError parsing file: {e}");
+                            continue;
+                        }
+                    };
+
+                    toml["package"]["version"] = value(version);
+
+                    match fs::write(&path, toml.to_string()) {
+                        Ok(_) => info!("\tUpdate successful!"),
+                        Err(e) => error!("\tError writing file: {e}"),
+                    }
+                }
+            },
+        }
+    }
+}
